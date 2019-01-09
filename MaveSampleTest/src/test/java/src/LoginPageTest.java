4bf39f29fdb4b6d47b1c0256b550ad48a6c8c9a6
arulmozhi.a <arulmozhi.a@10.110.173.124>
2019-01-09 07:26:04 +0530

    Commit 1

---
+++ MaveSampleTest/src/test/java/src/LoginPageTest.java
@@ -0,0 +1,21 @@
+package src;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+  @Test
+  public void f() throws Exception {
+	  WebDriver driver;
+	  driver=new ChromeDriver();
+	  
+	  driver.get("http://10.67.89.42/Common/Login.aspx");
+	  Thread.sleep(3000);
+	  String pageTitle=driver.getTitle();
+	  System.out.println(pageTitle);
+	  Assert.assertEquals(pageTitle,"In Essence Bank");
+	//  driver.quit();
+  }
+}
